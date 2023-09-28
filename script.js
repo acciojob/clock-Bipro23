39d16511ba4c66193a531e4c615f3bc1ca612c24
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-28 19:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//your JS code here. If required.
-//your code 
-
-setInterval(()=>{
-let hour = new Date().getHours();
-let min = new Date().getMinutes();
-let sec = new Date().getSeconds();
-
-let hourHand = ((30*hour) + (min/2) + 90);
-let minHand = (6 * min + 90);
-let secHand = (6 * sec);
-
-document.getElementsByClassName("hour-hand")[0].style.transform = `rotate(${hourHand}deg)`;
-document.getElementsByClassName("min-hand")[0].style.transform = `rotate(${minHand}deg)`;
-document.getElementsByClassName("second-hand")[0].style.transform = `rotate(${secHand}deg)`;
-},1000);
+function show(x,y)
+{
+    let count=0;
+    const t=setInterval(()=>{
+        const a=new Date();
+        const b=a.toLocaleString();
+        document.getElementById('timer').innerHTML = b;
+        count++;
+        if(count===y)
+            clearInterval(t)
+    },x*1000)
+}
+show(1,10)
